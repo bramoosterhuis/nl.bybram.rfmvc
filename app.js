5ecf4594bc328f90469a0400ba32f3e9695ca395
Bram Oosterhuis <bram.oosterhuis.nl@gmail.com>
2016-08-05 15:19:45 +0200

    looks like it  all works now

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 "use strict";
+var HomeyRadio = require('homey-arduinodriver');
+var radio = new HomeyRadio({'address':0x50});
 
 function init() {
 	Homey.log("nl.bybram.rfmvc running....");
@@ -6,19 +8,21 @@ function init() {
 
 // bind flow action
 Homey.manager('flow').on('action.set_speed', function( callback, args ){
-	callback( null, set_speed(args.speed.id));
+	set_speed(args.id, args.speed)
+	callback( null, true);
 });
 
-function set_speed(input) {
-	var HomeyRadio = require('homey-arduinodriver');
-	var radio = new HomeyRadio({'address':0x50});
-	var payload = new Buffer([parseInt(input)]);
+function set_speed(id ,speed) {
+	var payload = new Buffer([
+		parseInt(id),
+		parseInt(speed)
+	]);
 	radio.send(0x58, payload, function (err) {
 		if (!err) {
-			console.log("ok speed has been set");
+			Homey.log("Ventilation " + id + " set to: " + speed)
 			return true;
 		} else {
-			console.log("error setting speed");
+			Homey.log("Ventilation " + id + " ACK error")
 			return false;
 		}
 	});
